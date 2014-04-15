fef59ce9bf3515de421ea0377a72c2f1cdf82ec1
Albert Agram <albert.agram@gmail.com>
2014-04-15 11:56:52 +0000

    Demo simple routing in app

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
 require "tracks"
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require "quotes_controller"
 
 module BestQuotes
   class Application < Tracks::Application
