7875afcb825d2f0f27a827ff548a7f9e5938b1af
Albert Agram <albert.agram@gmail.com>
2014-04-16 19:56:17 +0000

    rails-like rendering

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,9 @@
 class QuotesController < Tracks::Controller
   def a_quote
-    "There is nothing either good or bad but thinking makes it so."
-    "\n<pre>\n#{env}\n</pre>"
+    render :a_quote, :noun => :winking
   end
 
-  def exception
-    raise "It's a bad one!!"
+  def user_agent
+    render :user_agent
   end
 end
