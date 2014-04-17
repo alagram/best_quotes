19ce934680d77193160896d8d92a6943b185125f
Albert Agram <albert.agram@gmail.com>
2014-04-17 21:04:03 +0000

    Use json to mimic a table

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,9 @@ class QuotesController < Tracks::Controller
   def user_agent
     render :user_agent
   end
+
+  def quote_1
+    quote_1 = Tracks::Model::FileModel.find(1)
+    render :quote, obj: quote_1
+  end
 end
