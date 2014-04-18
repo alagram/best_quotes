8aaf4a51cd386614625d7d51f8674e5ff0ef4d4d
Albert Agram <albert.agram@gmail.com>
2014-04-18 21:51:36 +0000

    Adding more json files, rendering index template and creating more quotes.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,22 @@ class QuotesController < Tracks::Controller
   end
 
   def quote_1
-    quote_1 = Tracks::Model::FileModel.find(1)
+    quote_1 = FileModel.find(1)
     render :quote, obj: quote_1
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create(attrs)
+    render :quote, obj: m
   end
 end
